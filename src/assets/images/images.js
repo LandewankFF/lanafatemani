0aee978442fd7af8dccb32860362f89939761551
LandewankFF <landewankfahrezaf@gmail.com>
2024-10-15 14:38:03 +0700

    feat(images): add new images for Culture, Tourism, Culinary, Souvenir, and Updated images.js

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -3,9 +3,14 @@ import ads from './ads.png'
 import manage from './manage.png'
 import food from './indonesia-padang-cuisine-2023-11-27-05-23-17-utc.jpg'
 import culture from './asia-family-pour-water-on-the-hands-of-revered-eld-2024-04-26-21-00-31-utc.jpg'
-
+import island from '../images/whats-better-than-one-island-high-angle-shot-of-t-2023-11-27-05-26-53-utc.jpg'
+import about from '../images/Left.png'
+import tourism from '../images/tourism.png'
+import souvenir from '../images/souvenir.png'
+import culinary from '../images/culinary.png'
+import dance from '../images/Culture2.png'
 const images = {
-    conten_creative, ads, manage, food, culture
+    conten_creative, ads, manage, food, culture,island, about, culinary,souvenir,tourism ,dance
 }
 
 export default images;
