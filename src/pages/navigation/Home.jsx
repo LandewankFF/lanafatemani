facca2335862b852ef2c869bb123bde7cb321b45
LandewankFF <landewankfahrezaf@gmail.com>
2024-10-16 09:26:39 +0700

    Update : remove script unimportant

--- src/pages/navigation/Home.jsx
+++ src/pages/navigation/Home.jsx
@@ -6,14 +6,6 @@ export default function Home() {
   return (
     <div>
       <Homepage/>
-
-    {/* <Price
-      ClassBg ='bg-black'
-      Title='Medium'  
-      Caption='Untuk meningkatkan views dan interaksi yang relate dengan audiens. Ideal untuk membangun brand awareness dan menjadikan destinasi wisata semakin menarik dengan konten visual sinematik' 
-      Price='Rp3.000.000'
-      ListCard={PackageStandard}
-    /> */}
     </div>
   );
 }
