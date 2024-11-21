65f02aecf0391a23c33ad7bde2948d3d9dfc543b
Landewank <21102120@ittelkom-pwt.ac.id>
2024-11-21 15:44:21 +0700

    Update: Add section video Galery

--- src/components/Portfolio Video/Portfoliovideo.jsx
+++ src/components/Portfolio Video/Portfoliovideo.jsx
@@ -1,17 +1,19 @@
 import Footer from "../ui/Footer";
 import HeaderNav from "../ui/HeaderNav"
 import Carousellogo from "./Carousellogo";
-import { Video } from "./Video"
+import { Video, VideoSosmed } from "./Video"
+
 
 const PortfolioPage = () => {
   return (
     <>
       <HeaderNav
-        Title="Portfolio"
+        Title="Portfolio Vidio"
         Caption="Melalui portofolio kami, Anda dapat melihat bagaimana inovasi dan kreativitas menyatu untuk memperkuat sektor pariwisata dan ekonomi kreatif. Kami telah bekerja sama dengan berbagai mitra untuk mengembangkan strategi iklan yang efektif, membantu meningkatkan eksposur dan daya tarik destinasi wisata, serta membangun brand yang kuat."
       />
       <Carousellogo/>
       <Video/>
+      <VideoSosmed/>
       <Footer/>
     </>
   )
